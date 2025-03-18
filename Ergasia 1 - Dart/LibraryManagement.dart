bd49fb8bd8525ef8f50fbce17494cd38587414cb
Nikolaos Vovos <nikolaos.vovos@prometric.com>
2025-03-18 17:41:41 +0000

    First commit

---
+++ Ergasia 1 - Dart/LibraryManagement.dart
@@ -0,0 +1,60 @@
+void main() {
+  var library = Library();
+  
+  library.insertBook(Book("Η Φόνισσα", ["Αλέξανδρος Παπαδιαμάντης"], "Λογοτεχνία", "978-960-425-059-2"));
+  library.insertBook(Book("Εραγκον", ["Κριστοφερ Παολινι"], "Λογοτεχνία", "978-990-456-059-3"));
+  library.insertBook(Book("Η Ανθρωπότητα", ["Γιούβαλ Νώε Χαράρι"], "Επιστημονικό", "978-960-507-027-4"));
+  library.insertBook(Book("Το Κεφάλαιο", ["Καρόλος Μαρξ"], "Οικονομία", "978-960-348-226-0"));
+
+  print("Όλα τα βιβλία στη βιβλιοθήκη:");
+  library.displayBooks();
+
+  print("\nΑναζήτηση με ISBN (978-960-507-027-4):");
+  print(library.searchByISBN("978-960-507-027-4") ?? "Δεν βρέθηκε");
+
+  print("\nΑναζήτηση με τίτλο (Το Κεφάλαιο):");
+  print(library.searchByTitle("Το Κεφάλαιο"));
+
+  print("\nΑναζήτηση με είδος (Λογοτεχνία):");
+  print(library.searchByGenre("Λογοτεχνία"));
+}
+
+class Book {
+  String title;
+  List<String> authors;
+  String genre;
+  String isbn;
+
+  Book(this.title, this.authors, this.genre, this.isbn);
+
+  @override
+  String toString() {
+    return 'Τίτλος: $title, Συγγραφείς: ${authors.join(", ")}, Είδος: $genre, ISBN: $isbn';
+  }
+}
+
+class Library {
+  List<Book> books = [];
+
+  void insertBook(Book book) {
+    books.add(book);
+  }
+
+  void displayBooks() {
+    for (var book in books) {
+      print(book);
+    }
+  }
+
+  Book? searchByISBN(String isbn) {
+    return books.cast<Book?>().firstWhere((book) => book?.isbn == isbn, orElse: () => null);
+  }
+
+  List<Book> searchByTitle(String title) {
+    return books.where((book) => book.title.toLowerCase().contains(title.toLowerCase())).toList();
+  }
+
+  List<Book> searchByGenre(String genre) {
+    return books.where((book) => book.genre.toLowerCase() == genre.toLowerCase()).toList();
+  }
+}
